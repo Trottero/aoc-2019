2bef0f7e1cf6c4f23babed0939a12bbb7f07d91a
Trottero <wrlxs@hotmail.com>
2019-12-09 15:22:41 +0100

    Copy old project to new AOC

---
+++ AOC-9-1/Amplifier.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace AOC_9_1
+{
+	public class Amplifier : IntCodeComputer
+	{
+		static string AmplifierProgram = "3,8,1001,8,10,8,105,1,0,0,21,34,59,68,85,102,183,264,345,426,99999,3,9,101,3,9,9,102,3,9,9,4,9,99,3,9,1002,9,4,9,1001,9,2,9,1002,9,2,9,101,5,9,9,102,5,9,9,4,9,99,3,9,1001,9,4,9,4,9,99,3,9,101,3,9,9,1002,9,2,9,1001,9,5,9,4,9,99,3,9,1002,9,3,9,1001,9,5,9,102,3,9,9,4,9,99,3,9,1001,9,1,9,4,9,3,9,1001,9,2,9,4,9,3,9,1002,9,2,9,4,9,3,9,102,2,9,9,4,9,3,9,1001,9,2,9,4,9,3,9,101,2,9,9,4,9,3,9,102,2,9,9,4,9,3,9,102,2,9,9,4,9,3,9,101,1,9,9,4,9,3,9,101,1,9,9,4,9,99,3,9,1002,9,2,9,4,9,3,9,1002,9,2,9,4,9,3,9,1002,9,2,9,4,9,3,9,1001,9,2,9,4,9,3,9,101,1,9,9,4,9,3,9,1001,9,1,9,4,9,3,9,1002,9,2,9,4,9,3,9,101,2,9,9,4,9,3,9,101,1,9,9,4,9,3,9,101,2,9,9,4,9,99,3,9,1001,9,1,9,4,9,3,9,101,1,9,9,4,9,3,9,102,2,9,9,4,9,3,9,1002,9,2,9,4,9,3,9,1002,9,2,9,4,9,3,9,101,2,9,9,4,9,3,9,1001,9,1,9,4,9,3,9,1001,9,1,9,4,9,3,9,102,2,9,9,4,9,3,9,1001,9,1,9,4,9,99,3,9,1002,9,2,9,4,9,3,9,101,1,9,9,4,9,3,9,1001,9,2,9,4,9,3,9,102,2,9,9,4,9,3,9,1002,9,2,9,4,9,3,9,102,2,9,9,4,9,3,9,1001,9,1,9,4,9,3,9,1002,9,2,9,4,9,3,9,1001,9,2,9,4,9,3,9,101,2,9,9,4,9,99,3,9,1001,9,1,9,4,9,3,9,1002,9,2,9,4,9,3,9,101,2,9,9,4,9,3,9,101,1,9,9,4,9,3,9,1002,9,2,9,4,9,3,9,101,1,9,9,4,9,3,9,102,2,9,9,4,9,3,9,101,1,9,9,4,9,3,9,102,2,9,9,4,9,3,9,101,2,9,9,4,9,99"; public Amplifier NextAmp { get; set; }
+		public string AmpId { get; set; }
+		public Amplifier() : base(AmplifierProgram)
+		{
+
+		}
+
+		public override void SendOutput(int x)
+		{
+			ProgramOutput = x;
+			NextAmp.AddNewVariable(x);
+		}
+
+		public void AddNewVariable(int variable)
+		{
+			lock (thisLock)
+			{
+				InputVariables.Add(variable);
+			}
+		}
+	}
+
+	public class AmplifierArray
+	{
+		private string[] AmpIds = new[] { "A", "B", "C", "D", "E" };
+		private readonly IEnumerable<int> phaseCodes;
+		private List<Amplifier> Amplifiers = new List<Amplifier>();
+
+		public AmplifierArray(List<int> PhaseCodes)
+		{
+			phaseCodes = PhaseCodes;
+			var Amplifiers = PhaseCodes.Select(
+				r => new Amplifier()
+			).ToList();
+
+			Amplifier prev = new Amplifier();
+			foreach (var a in Amplifiers)
+			{
+				prev.NextAmp = a;
+				prev = a;
+			}
+			Amplifiers[Amplifiers.Count() - 1].NextAmp = Amplifiers.First();
+
+			for (int i = 0; i < Amplifiers.Count(); i++)
+			{
+				Amplifiers[i].InputVariables.Add(PhaseCodes[i]);
+				Amplifiers[i].AmpId = AmpIds[i];
+			}
+			this.Amplifiers = Amplifiers;
+		}
+
+		public int Amplify(int input)
+		{
+			var ts = new List<Thread>();
+			Amplifiers[0].AddNewVariable(input);
+			Amplifiers.ForEach(a =>
+			{
+				var t = new Thread(new ThreadStart(a.StartComputer));
+				t.Name = "Computer_Thread";
+				t.Start();
+				ts.Add(t);
+			});
+			while (ts.Any(r => r.IsAlive))
+			{
+
+			}
+			return Amplifiers.Last().ProgramOutput;
+		}
+	}
+}
---
+++ AOC-9-1/IntCodeComputer.cs
@@ -0,0 +1,182 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace AOC_9_1
+{
+	public class IntCodeComputer
+	{
+		private static readonly int[] amountofArgs = { 0, 3, 3, 1, 1, 2, 2, 3, 3 };
+		protected object thisLock = new object();
+
+		private string input = "";
+		private int[] computerMemory;
+		public ICollection<int> InputVariables { get; set; } = new List<int> { };
+		private int InputIndex = 0;
+		public int position { get; set; } = 0;
+
+		public int ProgramOutput { get; set; } = -1;
+
+		public IntCodeComputer(string program)
+		{
+			input = program;
+			ClearMemory();
+		}
+
+		public void StartComputer()
+		{
+
+			// Start the execution of the program on pos 0
+			ExecuteInstruction(0);
+		}
+
+		public void ClearMemory()
+		{
+			computerMemory = input.Split(',').Select(int.Parse).ToArray();
+		}
+
+		public void ExecuteInstruction(int pos)
+		{
+			position = pos;
+			int skippings = 0;
+			// Expected to be atleast 2 numbers
+			var instruction = computerMemory[pos].ToString();
+			var instructionCode = int.Parse(instruction);
+			IList<char> parameterSettings = new List<char>();
+			if (instruction.Length > 1)
+			{
+				instructionCode = int.Parse(instruction.Substring(instruction.Length - 2, 2));
+				parameterSettings = instruction.Substring(0, instruction.Length - 2).ToCharArray().ToList();
+			}
+
+			if (instructionCode == 99)
+			{
+				return;
+			}
+
+			for (int i = parameterSettings.Count(); i < amountofArgs[instructionCode]; i++)
+			{
+				parameterSettings.Insert(0, '0');
+			}
+
+			parameterSettings = parameterSettings.Reverse().ToArray();
+
+			if (amountofArgs[instructionCode] == 1) // in and output
+			{
+				if (instructionCode == 3)
+				{
+					var shouldLoop = true;
+					while (shouldLoop) // Checks for new instructions
+					{
+						lock (thisLock)
+						{
+							shouldLoop = !InputVariables.Any();
+						}
+					}
+					computerMemory[computerMemory[pos + 1]] = InputVariables.First(); // Take the first variable and do something with it.
+					InputVariables.Remove(InputVariables.First()); // Remove the consumed variable, this way the next one is ready for the queue.
+				}
+
+				if (instructionCode == 4)
+				{
+					if (parameterSettings[0] == '1')
+					{
+						SendOutput(computerMemory[pos + 1]);
+					}
+					else
+					{
+						SendOutput(computerMemory[computerMemory[pos + 1]]);
+					}
+				}
+				skippings = 2;
+			}
+
+			if (amountofArgs[instructionCode] == 2 || amountofArgs[instructionCode] == 3) // Jump functions
+			{
+				int parm1 = 0;
+				if (parameterSettings[0] == '1')
+				{
+					parm1 = computerMemory[pos + 1];
+				}
+				else
+				{
+					parm1 = computerMemory[computerMemory[pos + 1]];
+				}
+
+				int parm2 = 0;
+				if (parameterSettings[1] == '1')
+				{
+					parm2 = computerMemory[pos + 2];
+				}
+				else
+				{
+					parm2 = computerMemory[computerMemory[pos + 2]];
+				}
+
+				if (instructionCode == 5 || instructionCode == 6)
+				{
+					skippings = 3;
+					if (instructionCode == 5 && parm1 != 0) // Jump 1
+					{
+						skippings = 0;
+						pos = parm2;
+					}
+					if (instructionCode == 6 && parm1 == 0) // Jump 2
+					{
+						skippings = 0;
+						pos = parm2;
+					}
+				}
+
+				if (instructionCode == 1) // Adding
+				{
+					computerMemory[computerMemory[pos + 3]] = parm2 + parm1;
+					skippings = 4;
+				}
+
+				if (instructionCode == 2) // Multiple
+				{
+					computerMemory[computerMemory[pos + 3]] = parm2 * parm1;
+					skippings = 4;
+				}
+
+				if (instructionCode == 7) // Less than
+				{
+					if (parm1 < parm2)
+					{
+						computerMemory[computerMemory[pos + 3]] = 1;
+					}
+					else
+					{
+						computerMemory[computerMemory[pos + 3]] = 0;
+					}
+
+					skippings = 4;
+				}
+
+				if (instructionCode == 8)
+				{
+					if (parm1 == parm2)
+					{
+						computerMemory[computerMemory[pos + 3]] = 1;
+					}
+					else
+					{
+						computerMemory[computerMemory[pos + 3]] = 0;
+					}
+
+					skippings = 4;
+				}
+			}
+
+			ExecuteInstruction(pos + skippings);
+		}
+		public virtual void SendOutput(int output)
+		{
+			ProgramOutput = output;
+		}
+	}
+
+}
