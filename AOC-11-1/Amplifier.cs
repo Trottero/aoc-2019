79c52173501af6b6c2c426a243eaa1528ef85e5b
Trottero <wrlxs@hotmail.com>
2019-12-12 01:35:57 +0100

    Day 11

---
+++ AOC-11-1/Amplifier.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace AOC_11_1
+{
+	public class Amplifier : IntCodeComputer
+	{
+		private const string AmplifierProgram = "3,52,1001,52,-5,52,3,53,1,52,56,54,1007,54,5,55,1005,55,26,1001,54,-5,54,1105,1,12,1,53,54,53,1008,54,0,55,1001,55,1,55,2,53,55,53,4,53,1001,56,-1,56,1005,56,6,99,0,0,0,0,10";
+		public Amplifier NextAmp { get; set; }
+		public string AmpId { get; set; }
+		public Amplifier(string ampProgram = AmplifierProgram) : base(ampProgram)
+		{
+		}
+
+		public override void SendOutput(long x)
+		{
+			ProgramOutput.Add(x);
+			NextAmp.AddNewVariable(x);
+		}
+
+		public void AddNewVariable(long variable)
+		{
+			lock (thisLock)
+			{
+				InputVariables.Add(variable);
+			}
+		}
+	}
+
+	public class AmplifierArray
+	{
+		private string[] AmpIds = new[] { "A", "B", "C", "D", "E" };
+		private readonly IEnumerable<int> _phaseCodes;
+		private readonly List<Amplifier> _amplifiers;
+
+		public AmplifierArray(List<int> PhaseCodes, string amplifierProgram = null)
+		{
+			_phaseCodes = PhaseCodes;
+			var Amplifiers = new List<Amplifier>();
+			if (string.IsNullOrEmpty(amplifierProgram))
+			{
+				Amplifiers = PhaseCodes.Select(
+					r => new Amplifier()
+				).ToList();
+			}
+			else
+			{
+				Amplifiers = PhaseCodes.Select(
+				   r => new Amplifier(amplifierProgram)
+			   ).ToList();
+			}
+
+			Amplifier prev = new Amplifier();
+			foreach (var a in Amplifiers)
+			{
+				prev.NextAmp = a;
+				prev = a;
+			}
+			Amplifiers[Amplifiers.Count() - 1].NextAmp = Amplifiers.First();
+
+			for (int i = 0; i < Amplifiers.Count(); i++)
+			{
+				Amplifiers[i].InputVariables.Add(PhaseCodes[i]);
+				Amplifiers[i].AmpId = AmpIds[i];
+			}
+			_amplifiers = Amplifiers;
+		}
+
+		public long Amplify(long input)
+		{
+			var ts = new List<Thread>();
+			_amplifiers[0].AddNewVariable(input);
+			_amplifiers.ForEach(a =>
+			{
+				var t = new Thread(new ThreadStart(a.StartComputer)) { Name = "Computer_Thread" };
+				t.Start();
+				ts.Add(t);
+			});
+			while (ts.Any(r => r.IsAlive))
+			{
+
+			}
+			return _amplifiers.Last().ProgramOutput.First();
+		}
+	}
+}
---
+++ AOC-11-1/IntCodeComputer.cs
@@ -0,0 +1,213 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace AOC_11_1
+{
+	public class IntCodeComputer
+	{
+		private static readonly int[] AmountofArgs = { 0, 3, 3, 1, 1, 2, 2, 3, 3, 1 };
+		protected object thisLock = new object();
+
+		private readonly string _input;
+		private long[] _computerMemory;
+
+		public ICollection<long> InputVariables { get; set; } = new List<long> { };
+		public long Position { get; set; } = 0;
+
+		public ICollection<long> ProgramOutput { get; set; } = new List<long>();
+
+		public long RelativeBase { get; set; } = 0;
+
+		public ComputerState State { get; set; } = ComputerState.RUNNING;
+
+		public IntCodeComputer(string program)
+		{
+			_input = program;
+			ClearMemory();
+		}
+
+		public void StartComputer()
+		{
+
+			// Start the execution of the program on pos 0
+			while (State != ComputerState.STOPPED)
+			{
+				ExecuteInstruction();
+			}
+		}
+
+		public void ClearMemory()
+		{
+			_computerMemory = _input.Split(',').Select(long.Parse).ToArray();
+		}
+
+		public void ExecuteInstruction()
+		{
+			var pos = Position;
+			long skippings = 0;
+			// Expected to be at least 2 numbers
+			var instruction = _computerMemory[pos].ToString();
+			var instructionCode = int.Parse(instruction);
+			IList<int> parameterSettings = new List<int>();
+
+
+			if (instruction.Length > 1)
+			{
+				instructionCode = int.Parse(instruction.Substring(instruction.Length - 2, 2));
+				parameterSettings = instruction.Substring(0, instruction.Length - 2)
+					.ToCharArray()
+					.Select(r => int.Parse(r.ToString()))
+					.ToList();
+			}
+
+			if (instructionCode == 99)
+			{
+				State = ComputerState.STOPPED;
+				return;
+			}
+
+			for (var i = parameterSettings.Count; i < AmountofArgs[instructionCode]; i++)
+			{
+				parameterSettings.Insert(0, 0);
+			}
+			parameterSettings = parameterSettings.Reverse().ToArray();
+
+			if (instructionCode == 3)
+			{
+				Memory(GetPosition(1, parameterSettings[0])) = GetInput();
+				skippings = 2;
+			}
+
+			if (instructionCode == 4)
+			{
+				SendOutput(GetValue(1, parameterSettings[0]));
+				skippings = 2;
+			}
+
+			if (instructionCode == 5 || instructionCode == 6)
+			{
+				skippings = 3;
+				if (instructionCode == 5 && GetValue(1, parameterSettings[0]) != 0) // Jump 1
+				{
+					skippings = 0;
+					pos = GetValue(2, parameterSettings[1]);
+				}
+				if (instructionCode == 6 && GetValue(1, parameterSettings[0]) == 0) // Jump 2
+				{
+					skippings = 0;
+					pos = GetValue(2, parameterSettings[1]);
+				}
+			}
+
+			if (instructionCode == 1) // Adding
+			{
+				Memory(GetPosition(3, parameterSettings[2])) = GetValue(1, parameterSettings[0]) + GetValue(2, parameterSettings[1]);
+				skippings = 4;
+			}
+
+			if (instructionCode == 2) // Multiple
+			{
+				Memory(GetPosition(3, parameterSettings[2])) = GetValue(1, parameterSettings[0]) * GetValue(2, parameterSettings[1]);
+				skippings = 4;
+			}
+
+			if (instructionCode == 7) // Less than
+			{
+				if (GetValue(1, parameterSettings[0]) < GetValue(2, parameterSettings[1]))
+				{
+					Memory(GetPosition(3, parameterSettings[2])) = 1;
+				}
+				else
+				{
+					Memory(GetPosition(3, parameterSettings[2])) = 0;
+				}
+
+				skippings = 4;
+			}
+
+			if (instructionCode == 8) // equals
+			{
+				if (GetValue(1, parameterSettings[0]) == GetValue(2, parameterSettings[1]))
+				{
+					Memory(GetPosition(3, parameterSettings[2])) = 1;
+				}
+				else
+				{
+					Memory(GetPosition(3, parameterSettings[2])) = 0;
+				}
+
+				skippings = 4;
+
+			}
+
+			if (instructionCode == 9)
+			{
+				RelativeBase += GetValue(1, parameterSettings[0]);
+				skippings = 2;
+			}
+
+			var b = RelativeBase;
+
+			Position = pos + skippings;
+		}
+
+		public virtual void SendOutput(long output)
+		{
+			ProgramOutput.Add(output);
+		}
+
+		public virtual long GetInput()
+		{
+			var shouldLoop = true;
+			while (shouldLoop) // Checks for new instructions
+			{
+				lock (thisLock)
+				{
+					shouldLoop = !InputVariables.Any();
+				}
+			}
+			var longtoret = InputVariables.First();
+			InputVariables.Remove(InputVariables.First()); // Remove the consumed variable, this way the next one is ready for the queue.
+			return longtoret; // Take the first variable and do something with it.
+		}
+		public long GetPosition(long parameterOffset, int parameterMode)
+		{
+			switch (parameterMode)
+			{
+				// Position mode
+				case 0:
+					return Memory(Position + parameterOffset);
+				// Direct interpretation mode
+				case 1:
+					return Position + parameterOffset;
+				case 2:
+					return RelativeBase + Memory(Position + parameterOffset);
+			}
+			return -1;
+		}
+
+		public long GetValue(long parameterOffset, int parameterMode)
+		{
+			return Memory(GetPosition(parameterOffset, parameterMode));
+		}
+
+		private ref long Memory(long index)
+		{
+			if (index > _computerMemory.Length - 1)
+			{
+				Array.Resize(ref _computerMemory, (int)index + 1);
+			}
+
+			return ref _computerMemory[index];
+		}
+
+		public enum ComputerState
+		{
+			RUNNING,
+			STOPPED
+		}
+	}
+}
